f96b93e96baaa22c6bde693c7e89d90d90897e59
Matheusdnf <matheusdiniz870@gmail.com>
2025-01-27 00:07:09 -0300

    feat - entrada de dados e validação do formulário de login

--- dispensa/src/app/components/navbar/page.js
+++ dispensa/src/app/components/navbar/page.js
@@ -1,7 +1,12 @@
 "use client";
-import footer_style from "@/app/style/footer.module.css";
-import form from "@/app/style/form.module.css";
+import {
+  validateEmail,
+  validatePassword,
+  handleChange,
+} from "@/app/lib/validations/page";
 import { useState } from "react";
+import footer_style from "@/app/style/footer.module.css";
+
 export function Navbar() {
   return (
     <nav className="navbar bg-body-none">
@@ -27,41 +32,120 @@ export function Navbar() {
   );
 }
 
-export function Form() {
-  return (
-    <div className={form.form}>
-      <h1>Entrar </h1>
-      <div>
-        <div className="input-group mb-3">
-          <input
-            type="text"
-            className="form-control w-100"
-            placeholder="Email"
-            aria-label="Username"
-            aria-describedby="basic-addon1"
-          />
-        </div>
+export function Form({ email, setEmail, password, setPassword }) {
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [hidepassword, sethidepassword] = useState(false);
+
+  const secretPassword = () => {
+    sethidepassword(!hidepassword);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let hasError = false;
+
+    if (!validateEmail(email)) {
+      setEmailError("Email inválido");
+      hasError = true;
+    } else {
+      setEmailError("");
+    }
+
+    if (!validatePassword(password)) {
+      setPasswordError(
         <div>
-          <input
-            type="password"
-            className="form-control w-100"
-            placeholder="Senha"
-            aria-label="Password"
-            aria-describedby="basic-addon1"
-          />
+          <ul>
+            <li>Ter no mínimo 6 caracteres</li>
+            <li>Número [1,2,3]</li>
+            <li>caracteres Especiais[!@#$%^&*]</li>
+            <li>Letra</li>
+          </ul>
         </div>
-        <div className="d-flex justify-content-between align-items-center mt-3">
-          <a href="#">Esqueci Minha Senha</a>
-          <button className="btn btn-primary mt-3" type="button">
-            Entrar
-          </button>
+      );
+      hasError = true;
+    } else {
+      setPasswordError("");
+    }
+
+    if (!hasError) {
+      console.log("Formulário enviado com sucesso!", { email, password });
+    }
+  };
+
+  return (
+    <div className="form-container">
+      <form onSubmit={handleSubmit}>
+        <h1>Entrar</h1>
+        <div>
+          <div className="input-group mb-3">
+            <input
+              type="text"
+              className="form-control w-100"
+              placeholder="Email"
+              onChange={(e) => handleChange(e, setEmail)}
+              value={email}
+            />
+            {emailError && <div className="text-danger">{emailError}</div>}
+          </div>
+
+          <div className="input-group mb-3">
+            <input
+              type={hidepassword ? "text" : "password"}
+              className="form-control w-100"
+              placeholder="Senha"
+              onChange={(e) => handleChange(e, setPassword)}
+              value={password}
+            />
+
+            <button
+              type="button"
+              onClick={secretPassword}
+              style={{
+                marginLeft: "8px",
+                padding: "6px 12px",
+                cursor: "pointer",
+              }}
+            >
+              {hidepassword ? (
+                <img
+                  src="/img/olho-aberto.png"
+                  alt="Ocultar senha"
+                  width="24"
+                  height="24"
+                />
+              ) : (
+                <img
+                  src="/img/olho.png"
+                  alt="Exibir senha"
+                  width="24"
+                  height="24"
+                />
+              )}
+            </button>
+            <div>
+              {passwordError && (
+                <div className="text-danger">{passwordError}</div>
+              )}
+            </div>
+          </div>
+          <div className="d-flex justify-content-between align-items-center mt-3">
+            <a href="#">Esqueci minha senha</a>
+            <button className="btn btn-primary mt-3" type="submit">
+              Entrar
+            </button>
+          </div>
         </div>
-      </div>
+      </form>
     </div>
   );
 }
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div className="d-flex flex-column min-vh-100">
       <div className="d-flex flex-row flex-fill">
@@ -75,7 +159,12 @@ export default function Login() {
         </div>
         <div className="col-12 col-lg-6 d-flex flex-column justify-content-center p-4">
           <Navbar />
-          <Form />
+          <Form
+            email={email}
+            setEmail={setEmail}
+            password={password}
+            setPassword={setPassword}
+          />
         </div>
       </div>
       <Footer />
