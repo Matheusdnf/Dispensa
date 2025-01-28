e2aeb641d39ba0da5184eefd11a052594842ee41
lleusxam <blkleusxam@hotmail.com>
2025-01-27 23:50:33 -0300

    feat: importação do javascript do bootstrap

--- dispensa/src/app/layout.js
+++ dispensa/src/app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
