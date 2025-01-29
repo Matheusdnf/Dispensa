b74fe65790b2241399590c428048dad1d88cab3f
Matheusdnf <matheusdiniz870@gmail.com>
2025-01-29 15:38:57 -0300

    feat - adição de dropdown

---
+++ dispensa/src/app/components/dropdown.js
@@ -0,0 +1,64 @@
+import Link from "next/link";
+export function Dropdown_Pentries() {
+  return (
+    <div className="dropdown">
+      <button
+        className="btn btn-secondary dropdown-toggle"
+        type="button"
+        data-bs-toggle="dropdown"
+        aria-expanded="false"
+      >
+        Ações Dispensa
+      </button>
+      <ul className="dropdown-menu">
+        <li>
+          <Link href={"./pantries/new"} className="dropdown-item ">
+            Cadastrar Dispensa
+          </Link>
+        </li>
+        <li>
+          <Link href={"#"} className="dropdown-item">
+            Editar Dispensa
+          </Link>
+        </li>
+        <li>
+          <Link className="dropdown-item" href="#">
+            Excluir Dispensa
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export function Dropdown_Products() {
+  return (
+    <div className="dropdown">
+      <button
+        className="btn btn-secondary dropdown-toggle"
+        type="button"
+        data-bs-toggle="dropdown"
+        aria-expanded="false"
+      >
+        Ações Dispensa
+      </button>
+      <ul className="dropdown-menu">
+        <li>
+          <Link href={"./products/new"} className="dropdown-item ">
+            Cadastrar Produto
+          </Link>
+        </li>
+        <li>
+          <Link href={"#"} className="dropdown-item">
+            Editar Produto
+          </Link>
+        </li>
+        <li>
+          <Link className="dropdown-item" href="#">
+            Excluir Produto
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
