5c393f18ae63f4cb24bde42721f7277a21d5c703
Matheusdnf <matheusdiniz870@gmail.com>
2025-01-29 15:40:26 -0300

    feat - função de informação inválida

---
+++ dispensa/src/app/components/not_information.js
@@ -0,0 +1,10 @@
+export function Not_information({ menssage }) {
+  return (
+    <div
+      className="d-grid justify-items-center align-items-center"
+      style={{ height: "100vh" }}
+    >
+      <h1 className="text-center mb-32">{menssage}</h1>
+    </div>
+  );
+}
