7d641fdb472fc49d97b200171df5ae85bdf6c144
Matheusdnf <matheusdiniz870@gmail.com>
2025-01-30 00:38:07 -0300

    feat - resolução do problema de carregamento de imagem padrão

--- dispensa/src/app/components/showCard.js
+++ dispensa/src/app/components/showCard.js
@@ -7,14 +7,11 @@ import { Modal_function, Modal_function_pratries } from "./modal";
 export function ShowCard({ itens, ismodal, button_pantries }) {
   // Manipuladores de Evento
   const [hoveredIndex, setHoveredIndex] = useState(null);
-  const [imgSrc, setImgSrc] = useState("");
+  const [imgSrc, setImgSrc] = useState("/img/invalid.jpg"); // Definir a imagem padrão diretamente
 
   // Caso a imagem apresente erro, chama essa função
-  const handleError = (index) => {
-    setImgSrc((prev) => ({
-      ...prev,
-      [index]: "/img/invalid.jpg",
-    }));
+  const handleError = () => {
+    setImgSrc("/img/invalid.jpg");
   };
 
   const handleMouseEnter = (index) => {
@@ -33,28 +30,23 @@ export function ShowCard({ itens, ismodal, button_pantries }) {
     <div className="d-flex flex-wrap justify-content-around">
       {itens.map((p, index) => (
         <div
-          // Interpolação de string ${}$ -- diferentes classes CSS podem ser aplicadas dependendo da situação
           className={`card m-3 ${card.card} ${
-            // Caso essa comparação seja atendida, o efeito de hover é aplicado
             hoveredIndex === index ? card.cardHovered : ""
           }`}
           key={index}
-          // Está em cima do mouse
           onMouseEnter={() => handleMouseEnter(index)}
-          // Não está em cima do mouse
           onMouseLeave={handleMouseLeave}
         >
           <img
-            src={imgSrc[index] || p.img}
+            src={p.img || imgSrc} // Usar a imagem original se existir, caso contrário, usar a imagem padrão
             className="card-img-top"
             alt={p.name}
-            onError={() => handleError(index)}
+            onError={handleError} // Chamar handleError se a imagem falhar ao carregar
           />
           <div className="card-body">
             <h5 className="card-title">{p.name}</h5>
             <p className="card-text m-1">{p.description}</p>
             {p.validate && <p className="card-text">Validade: {p.validate}</p>}
-            {/* Rota que irá levar para os produtos específicos */}
             {ismodal ? (
               <Modal_function pantryId={p.id} productId={p.productId} />
             ) : (
