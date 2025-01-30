970f60686aebd16102645761a0f78ea15e3780e6
Matheusdnf <matheusdiniz870@gmail.com>
2025-01-29 22:07:12 -0300

    feat - add modal patries

--- dispensa/src/app/components/modal.js
+++ dispensa/src/app/components/modal.js
@@ -7,6 +7,7 @@ import Link from "next/link";
 export function Modal_function() {
   const [showMainModal, setShowMainModal] = useState(false);
   const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const [successmensage, setsuccessmensage] = useState("");
 
   // Funções para abrir/fechar o modal principal
   const handleCloseMainModal = () => setShowMainModal(false);
@@ -19,8 +20,12 @@ export function Modal_function() {
   // Função para confirmar a exclusão
   const handleConfirmDelete = () => {
     console.log("Produto excluído!"); // Lógica para excluir o produto
-    handleCloseDeleteModal(); // Fecha o modal de exclusão
-    handleCloseMainModal(); // Fecha o modal principal
+    setsuccessmensage("Exclusão Realizado com Sucesso!");
+    setTimeout(() => {
+      setsuccessmensage("");
+      handleCloseDeleteModal(); // Fecha o modal de exclusão
+      handleCloseMainModal(); // Fecha o modal principal
+    }, 2000);
   };
 
   return (
@@ -88,6 +93,110 @@ export function Modal_function() {
             Excluir
           </Button>
         </Modal.Footer>
+        {successmensage && (
+          <div className=" d-flex alert alert-success mt-3">
+            {successmensage}
+          </div>
+        )}
+      </Modal>
+    </>
+  );
+}
+
+export function Modal_function_pratries() {
+  const [showMainModal, setShowMainModal] = useState(false);
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const [successmensage, setsuccessmensage] = useState("");
+
+  // Funções para abrir/fechar o modal principal
+  const handleCloseMainModal = () => setShowMainModal(false);
+  const handleShowMainModal = () => setShowMainModal(true);
+
+  // Funções para abrir/fechar o modal de exclusão
+  const handleCloseDeleteModal = () => setShowDeleteModal(false);
+  const handleShowDeleteModal = () => setShowDeleteModal(true);
+
+  // Função para confirmar a exclusão
+  const handleConfirmDelete = () => {
+    console.log("Dispensa excluída!"); // Lógica para excluir o produto
+    setsuccessmensage("Exclusão Realizado com Sucesso!");
+    setTimeout(() => {
+      setsuccessmensage("");
+      handleCloseDeleteModal(); // Fecha o modal de exclusão
+      handleCloseMainModal(); // Fecha o modal principal
+    }, 2000);
+  };
+
+  return (
+    <>
+      {/* Botão para abrir o modal principal */}
+      <Button variant="primary" onClick={handleShowMainModal}>
+        Realizar Alterações Na Dispensa
+      </Button>
+
+      {/* Modal Principal */}
+      <Modal show={showMainModal} onHide={handleCloseMainModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>[Nome da Dispensa]</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="m-6 bg-clip-padding">
+            <p>Você Deseja Editar a Dispensa:</p>
+            <Link href={"./pantries/edit"}>
+              <Button
+                variant="btn custom-button"
+                onClick={handleCloseMainModal}
+                className={`btn btn-secondary ms-3 ${style.btn_custom}`}
+              >
+                Editar
+              </Button>
+            </Link>
+          </div>
+          <div className="m-6 bg-clip-padding">
+            <p>Você Deseja Excluir a Dispensa:</p>
+            <Button
+              variant="btn btn-danger"
+              onClick={handleShowDeleteModal} // Abre o modal de exclusão
+              className="ms-3"
+            >
+              Excluir
+            </Button>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseMainModal}>
+            Fechar
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+      {/* Modal de Exclusão */}
+      <Modal
+        show={showDeleteModal}
+        onHide={handleCloseDeleteModal}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Confirmar Exclusão</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Tem certeza de que deseja excluir esta dispensa? Esta ação não pode
+          ser desfeita.
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseDeleteModal}>
+            Cancelar
+          </Button>
+          <Button variant="danger" onClick={handleConfirmDelete}>
+            Excluir
+          </Button>
+        </Modal.Footer>
+        {successmensage && (
+          <div className=" d-flex alert alert-success mt-3">
+            {successmensage}
+          </div>
+        )}
       </Modal>
     </>
   );
