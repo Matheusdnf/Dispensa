8121ed9413bd5332b24930abc79bde9c8b0abec7
Matheusdnf <matheusdiniz870@gmail.com>
2025-01-29 23:34:49 -0300

    feat - nova tela inicial

--- dispensa/src/app/page.js
+++ dispensa/src/app/page.js
@@ -1,14 +1,40 @@
 "use client";
 import { Footer } from "./components/footer";
 import { Navbar } from "./components/navbar";
+import style from "@/app/style/home.module.css";
+import Link from "next/link";
 export default function Home() {
   return (
     <div>
       <Navbar />
-      <h1>Bem Vindo !!</h1>
-      <span>
-        Esse projeto se trata de uma Dispensa Compartilhada, a qual é{" "}
-      </span>
+      <div className=" d-flex flex-column justify-content-center  text-center">
+        <h1 className={` ${style.custom_spacing}`}> Bem Vindo !!</h1>
+        <div className={` ${style.box}`}>
+          <span className={` ${style.span}`}>
+            Esse projeto trata-se de uma Dispensa, a qual é possível definir
+            caractéristicas dela e personalizá-lá, além de adicionar produtos
+            diversos a mesma, e assim ter um controle daquilo que se tem
+            estocado no recinto em questão.
+          </span>
+        </div>
+        <div className={` ${style.button}`}>
+          <Link
+            href={"/auth/register"}
+            type="button"
+            className="btn btn-outline-primary"
+          >
+            Criar Uma Conta
+          </Link>
+          <Link
+            href={"/auth/login"}
+            type="button"
+            className="btn btn-outline-primary"
+          >
+            Logar
+          </Link>
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 }
